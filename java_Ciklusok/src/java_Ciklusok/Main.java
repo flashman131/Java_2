8dcb4b0c31b0c479e2d2a5c9708a0701da4c02ad
Balázs Petró <flashman131@gmail.com>
2018-10-10 18:08:35 +0200

    1. feladat for

---
+++ java_Ciklusok/src/java_Ciklusok/Main.java
@@ -0,0 +1,13 @@
+package java_Ciklusok;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		for(int i=0; i<args.length;i++ )
+		{
+			System.out.println(args[i]);
+		}	
+	
+	}
+
+}
